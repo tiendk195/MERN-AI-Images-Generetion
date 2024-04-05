e5eae339a0413fc25694efc5174132416cfb5636
tiendk195 <tiendk195@gmail.com>
2024-04-05 21:15:54 +0700

    style: :hammer: Update route for dalle controller

--- backend/controllers/dalleController.js
+++ backend/controllers/dalleController.js
@@ -1,9 +1,31 @@
-import express from "express";
-import { getInfo, generateImage } from "./../controllers/dalleController.js";
+import * as dotenv from "dotenv";
+import { Configuration, OpenAIApi } from "openai";
 
-const router = express.Router();
+dotenv.config();
 
-router.get("/", getInfo);
-router.post("/", generateImage);
+export const getInfo = async (req, res) => {
+  res.status(200).json({ message: "Hello from DALL-E!" });
+};
 
-export default router;
+//create dalle image
+export const generateImage = async (req, res) => {
+  try {
+    const { prompt } = req.body;
+    const configuration = new Configuration({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+    const openai = new OpenAIApi(configuration);
+    const response = await openai.createImage({
+      prompt,
+      n: 1,
+      size: "1024x1024",
+      response_format: "b64_json",
+    });
+    const image = response.data.data[0].b64_json;
+    res.status(200).json({ photo: image });
+  } catch (error) {
+    res
+      .status(500)
+      .send(error?.response.data.error.message || "Something went wrong");
+  }
+};
